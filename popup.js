cf91cc84aa681859662cf3df55fceea8024d1868
henrilaats <henrilaats55@gmail.com>
2020-11-29 20:38:44 +0200

    viide javascriptile
    youtube link javascriptile

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+/*https://www.youtube.com/watch?v=MBaw_6cPmAw Link javascriptile, mida kasutasime popupide tegemiseks */
+
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
